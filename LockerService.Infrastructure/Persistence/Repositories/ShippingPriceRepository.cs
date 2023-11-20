e814f1b9155c92c1f73feda22e676cf5b8ba7b67
leson131002 <leson131002@gmail.com>
2023-11-20 21:55:14 +0700

    Fix shipping distance

--- LockerService.Infrastructure/Persistence/Repositories/ShippingPriceRepository.cs
+++ LockerService.Infrastructure/Persistence/Repositories/ShippingPriceRepository.cs
@@ -48,6 +48,6 @@ public class ShippingPriceRepository : BaseRepository<ShippingPrice>, IShippingP
         var d2 = lat2 * (Math.PI / 180.0);
         var num2 = long2 * (Math.PI / 180.0) - num1;
         var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-        return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+        return 6371 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
     }
 }
